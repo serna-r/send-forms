0223424972853cc22d9fb58a6cd9eed6480e55d4
serna-r <roisanch@pa.uc3m.es>
2024-12-10 13:32:00 +0100

    update to work with languages names and links

--- config.example.py
+++ config.example.py
@@ -4,3 +4,17 @@ class user_config:
     
 class emails_config:
     RECIPIENTS_FILENAME = "emails.xlsx"
+
+    class english:
+        SUBJECT = "Test"
+        LINKS = ['1', '2', '3']
+        BODY = """Dear $name
+        Test message
+        $link"""
+    
+    class spanish:
+        SUBJECT = "Prueba"
+        LINKS = ['4', '5', '6']
+        BODY = """Estimado $nombre
+        Mensaje de prueba
+        $link"""
--- send-email.py
+++ send-email.py
@@ -3,28 +3,44 @@ from email.mime.text import MIMEText
 from config import user_config, emails_config
 import pandas as pd
 
-# Get subject and body
-subject = "Email Subject"
-body = "This is the body of the text message"
-
 # Get sender
 sender = user_config.USER
 password = user_config.PASSWORD
 
+# Get subject and body for Spanish and English
+subject_es = emails_config.spanish.SUBJECT
+links_es = emails_config.spanish.LINKS
+body_es = emails_config.spanish.BODY
+
+subject_en = emails_config.english.SUBJECT
+links_en = emails_config.english.LINKS
+body_en = emails_config.english.BODY
+
 # Get recipients
-emails_df = pd.read_excel(emails_config.RECIPIENTS_FILENAME, header=None, names=['recipients'])
-recipients = emails_df['recipients'].to_list()
+emails_df = pd.read_excel(emails_config.RECIPIENTS_FILENAME, header=None, names=['names', 'emails', 'empty', 'language'])
+recipients_es = emails_df[pd.isna(emails_df['language'])]
+recipients_en = emails_df[emails_df['language'] == 'EN']
 
 # Send e-mail function
-def send_email(subject, body, sender, recipients, password):
+def send_email(subject, body, sender, recipient, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
+    msg['To'] = recipient['emails']
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipient['emails'], msg.as_string())
+    print(f"Message sent to {recipient['names']} ({recipient['emails']})")
 
-# Call send e-mail function
-send_email(subject, body, sender, recipients, password)
+# Prepare personalized emails
+def prepare_and_send_emails(subject, body_template, sender, recipients, links, password):
+    for i, recipient in recipients.iterrows():
+        # Replace placeholders with personalized data
+        personalized_body = body_template.replace('$name', recipient['names']).replace('$link', links[i%len(links)])
+        send_email(subject, personalized_body, sender, recipient, password)
+
+# Send Spanish emails
+prepare_and_send_emails(subject_es, body_es, sender, recipients_es, links_es, password)
+
+# Send English emails
+prepare_and_send_emails(subject_en, body_en, sender, recipients_en, links_en, password)
